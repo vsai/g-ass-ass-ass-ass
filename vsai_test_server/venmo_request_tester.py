4acd25882c5632df21638609274944c394fa4589
Vishalsai Daswani <vishalsai@seas.1923.apng.wireless-pennnet.upenn.edu>
2013-09-07 02:52:44 -0400

    swag master has come into play - PUSHUP TIME

---
+++ vsai_test_server/venmo_request_tester.py
@@ -0,0 +1,28 @@
+#venmo tester
+
+from flask import Flask, url_for, request, g, jsonify, abort, make_response, render_template
+import json
+
+app = Flask(__name__)
+app.logger.setLevel('INFO')
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
+
+@app.route('/')
+def hello_world():
+	return "Hello World"
+
+@app.route('/mkpay', methods=['POST'])
+def make_payment():
+	x = json.loads(request.data)
+	return "YOLO"
+
+def fake_make_charge():
+	x = {}
+	x['access_token'] = atoken
+	x['phone'] = 2012146067 #mochi
+	x['note'] = 'yolo'
+	x['amount'] = -1 #charge
+	
+
+
+
