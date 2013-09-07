aa77d96b50c2c66f51acec7137757514b9df847c
Justin Greet <jgreet@andrew.cmu.edu>
2013-09-07 04:41:06 -0400

    Finish navigation setup.

--- Vamos/assets/www/js/GMapsPage.js
+++ Vamos/assets/www/js/GMapsPage.js
@@ -1,14 +1,24 @@
 var GMapsPage = function(app){
 	this.app = app;
-	this.string = "GMaps";
+	this.string = "Middle";
+    this.navHTML = "<li class='tab onlyTwoTabs'><a href='#edit'>Edit</a></li>" + 
+				"<li class='tab onlyTwoTabs'><a href='#GMapsMain'>Google Maps Main</a></li>";
 }
 
 GMapsPage.prototype = {
 	// Event Handler for clicks
 	enter: function() {
 		$("#Middle").css("display", "block");
-		$("#tmpMiddle").html('On GMaps Page');
 		//Handle tabs to show
+        
+        //Now we need to set up the nav bar and the appropriate pages
+		$("#GMapsMain").css("display", "block");
+		$("#edit").css("display", "block");
+		//Now make sure the nav appears
+		$("#nav").css("display", "block");
+        $("#nav").append(this.navHTML);
+		//Set up easy tabs!
+		$("#Middle").easytabs();
 	},
 
 	exit: function() {
--- Vamos/assets/www/js/GPSPage.js
+++ Vamos/assets/www/js/GPSPage.js
@@ -1,12 +1,14 @@
 var GPSPage = function(app){
 	this.app = app;
 	this.string = "Middle";
+    this.navHTML = "<li class='tab'><a href='#edit'>Edit</a></li>" + 
+				"<li class='tab'><a href='#GPSMain'>GPS Main</a></li>" + 
+				"<li class='tab'><a href='#travelLog'>Travel Log</a></li>";
 }
 
 GPSPage.prototype = {
 	enter: function() {
 		$("#Middle").css("display", "block");
-		$("#tmpMiddle").html('On GPS Page');
 		// Handle tabs to show
 		
 		//Now we need to set up the nav bar and the appropriate pages
@@ -15,6 +17,7 @@ GPSPage.prototype = {
 		$("#travelLog").css("display", "block");
 		//Now make sure the nav appears
 		$("#nav").css("display", "block");
+        $("#nav").append(this.navHTML);
 		//Set up easy tabs!
 		$("#Middle").easytabs();
 	},
